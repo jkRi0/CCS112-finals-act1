e03b6f1cf8ba4eaa8e0dae50e6d92263a8ee9405
jkRi0 <rioverosjustine92@gmail.com>
2025-04-30 12:20:27 +0800

    added contacts

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
       <div>
-        <p>Email: youremail@example.com</p>
+        <p>Email: rioverosjustine92@gmail.com</p>
         <a href="https://github.com/yourusername">GitHub</a><br />
         <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
       </div>
